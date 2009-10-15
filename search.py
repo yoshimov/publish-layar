d97043af85abaeffb177f3d1b6206cad4997ea2c
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2009-10-15 16:45:05 -0700

    update using georss url. using property in the contents.

--- search.py
+++ search.py
@@ -3,6 +3,7 @@
 # Copyright 2009 yoshimov
 
 import math
+import re
 import wsgiref.handlers
 from django.utils import simplejson
 from google.appengine.ext import webapp
@@ -15,6 +16,7 @@ class SearchHandler(webapp.RequestHandler):
 	layername = self.request.get("layerName", default_value="yoshimovucsb")
 	reqlat = float(self.request.get("lat", default_value="0"))
 	reqlon = float(self.request.get("lon", default_value="0"))
+	radius = float(self.request.get("radius", default_value="500"))/50000
 	# Search key
 	searchkey = self.request.get("SEARCHBOX", default_value=None)
 	maps = []
@@ -24,7 +26,7 @@ class SearchHandler(webapp.RequestHandler):
 	  maps = db.GqlQuery("SELECT * FROM Mapdata WHERE mapname=:1", layername)
 	elif reqlat != 0:
 	  # filter by lat
-	  maps = db.GqlQuery("SELECT * FROM Mapdata WHERE mapname=:1 and lat<:2 and lat>:3", layername, reqlat + 0.02, reqlat - 0.02)
+	  maps = db.GqlQuery("SELECT * FROM Mapdata WHERE mapname=:1 and lat<:2 and lat>:3", layername, reqlat + radius, reqlat - radius)
 	else:
 	  # first 20 only (for test only)
 	  maps = db.GqlQuery("SELECT * FROM Mapdata WHERE mapname=:1 limit 20", layername)
@@ -32,22 +34,47 @@ class SearchHandler(webapp.RequestHandler):
 	hotspots = []
 	for map in maps:
 	  if searchkey != None:
-		if map.title.lower().find(searchkey.lower()) < 0:
+		if map.title.lower().find(searchkey.lower()) < 0 and (map.content == None or map.content.lower().find(searchkey.lower()) < 0):
 		  continue
 	  item = {"actions": [], "attribution": None, "imageURL": None,
 		"line2": None, "line3": None, "line4": None, "type": 0}
 	  item["id"] = "item%d" % map.key().id()
 	  if reqlat != 0:
-	    item["distance"] = math.sqrt((map.lat - reqlat)**2 + (map.lon - reqlon)**2)
+	    item["distance"] = math.sqrt((map.lat - reqlat)**2 + ((map.lon - reqlon)*1.1)**2)
 	  else:
 	    item["distance"] = 0
-	  item["title"] = map.title
-	  item["line2"] = map.content
-	  item["lat"] = long(map.lat * pow(10, 6))
-	  item["lon"] = long(map.lon * pow(10, 6))
+	  if map.content != None:
+		item["attribution"] = ""
+		for line in map.content.splitlines():
+		  if line.startswith("2:"):
+			item["line2"] = line[2:]
+		  elif line.startswith("3:"):
+			item["line3"] = line[2:]
+		  elif line.startswith("4:"):
+			item["line4"] = line[2:]
+		  elif line.startswith("image:"):
+			item["imageURL"] = line[6:]
+		  elif line.startswith("tel:"):
+			action = {"uri": line, "label": "Call this spot"}
+			item["actions"].append(action)
+		  elif line.startswith("mailto:"):
+			action = {"uri": line, "label": "Send mail"}
+			item["actions"].append(action)
+		  elif line.startswith("http:"):
+			action = {"uri": line, "label": "More info"}
+			item["actions"].append(action)
+		  else:
+			item["attribution"] = item["attribution"] + line + "\n"
+	  if re.match("\d:", map.title):
+		item["type"] = int(map.title[0])
+		item["title"] = map.title[2:]
+	  else:
+		item["title"] = map.title
+	  item["lat"] = long(map.lat * 10**6)
+	  item["lon"] = long(map.lon * 10**6)
 	  hotspots.append(item)
 	hotspots.sort(cmp=lambda x, y: cmp(x["distance"], y["distance"]))
-	if len(hotspots) >= 20:
+	if len(hotspots) > 20:
 	  del hotspots[20:]
 
 	ret = {"hotspots": hotspots, "layer": layername,
--- update.py
+++ update.py
@@ -4,7 +4,9 @@
 
 import wsgiref.handlers
 from django.utils import simplejson
+import feedparser
 
+from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext import db
 from mapdata import *
@@ -12,11 +14,57 @@ from mapdata import *
 class UpdateHandler(webapp.RequestHandler):
 
   def get(self):
-    map = Mapdata()
-    map.mapname = "yoshimovucsb"
-    map.title = "Harold Frank Hall"
-    map.content = "sample"
-    map.lat = 34.413853
-    map.lon = -119.841209
-    map.put()
-    self.response.out.write("write done")
+	# authentication
+	if not users.is_current_user_admin():
+	  self.redirect(users.create_login_url(self.request.url))
+	  return
+	self.response.out.write("""
+<html>
+<body>
+Please specify georss url:
+<form method="post">
+<p>
+Layer name:
+<input type="text" name="layername" />
+</p>
+<p>
+Georss url:
+<input type="text" name="url" />
+</p>
+<input type="submit" name="post" />
+</form>
+</body>
+</html>
+""")
+
+  def post(self):
+	# authentication
+	if not users.is_current_user_admin():
+	  self.redirect(users.create_login_url(self.request.url))
+	  return
+	layername = self.request.get("layername")
+	url = self.request.get("url")
+
+	# clear existing data
+	keys = db.GqlQuery("SELECT __key__ FROM Mapdata WHERE mapname=:1", layername).fetch(200)
+	db.delete(keys)
+	
+	feed = feedparser.parse(url)
+	self.response.out.write("<p>feed title: %s</p>" % feed.channel.title)
+
+	for item in feed.entries:
+	  self.response.out.write("<p><li>title: %s</li>" % item.title)
+	  self.response.out.write("<li>descrption: %s</li>" % item.description)
+
+	  points = item.point.split(" ")
+	  lat = float(points[0])
+	  lon = float(points[1])
+	  self.response.out.write("<li>geopoint: %f,%f</li></p>" % (lat, lon))
+	  map = Mapdata()
+	  map.mapname = layername
+	  map.title = item.title
+	  map.content = item.description
+	  map.lat = lat
+	  map.lon = lon
+	  map.put()
+	self.response.out.write("update POIs done")
